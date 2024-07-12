75970213a065cabea68f8d010d5b8a2df19c3a9b
curs3m4rk <pujyapreyans35@gmail.com>
2024-07-13 01:29:05 +0530

    Build our own React js App using npm, parcel, and other packages

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     {id: "parent"},
     [React.createElement(
         "div",
         { id: "child"},
-        [React.createElement("h1", {}, "This is a H1 Tag"), React.createElement("h2", {}, "This is a h2 Tag")]
+        [React.createElement("h1", {}, "This is a H1 Tag"), React.createElement("h2", {}, "Real-time txt change")]
     ),
     React.createElement(
         "div",
